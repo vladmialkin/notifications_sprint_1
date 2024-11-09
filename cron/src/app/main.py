8e7b0fb7b6fae052100ce1201e499df3cad1a233
vladmialkin <myalkin.vlad@yandex.ru>
2024-11-09 19:27:19 +0300

    feat: Добавление выполнения запроса к сервису notification.

--- cron/src/app/main.py
+++ cron/src/app/main.py
@@ -1,21 +1,34 @@
 import asyncio
 
+import aiohttp
 from app.db.postgres import connect_to_db
 
 
-async def check_messages(conn):
-    result = await conn.execute(
+async def fetch_notifications(conn):
+    result = await conn.fetch(
         """SELECT * FROM notifications 
         WHERE datetime_to_send 
         BETWEEN NOW() AND NOW() + INTERVAL '10 minutes';"""
     )
+    return result
 
-    print(result)
+
+async def send_notifications(notifications):
+    notification_url = "http://notification_producer:8090/api/v1/notifications/deferred_notification"
+
+    async with aiohttp.ClientSession() as session:
+        for notification in notifications:
+            async with session.post(notification_url, json=dict(notification)) as response:
+                if response.status == 200:
+                    print("Уведомление успешно отправлено:", await response.json())
+                else:
+                    print("Ошибка отправки уведомления:", response.status, await response.text())
 
 
 async def main():
     connection = await connect_to_db()
-    await check_messages(connection)
+    notifications =await fetch_notifications(connection)
+    await send_notifications(notifications)
 
 
 if __name__ == '__main__':
